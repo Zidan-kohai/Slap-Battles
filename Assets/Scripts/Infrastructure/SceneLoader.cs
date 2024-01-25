2a9b29f866d8ef402c08dad793a46f44704d4d74
Dmitriy Storozhuk <dreymentov@mail.ru>
2024-01-25 15:11:32 +0300

    fix animation

---
+++ Assets/Scripts/Infrastructure/SceneLoader.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader
+{
+    public void LoadSscene(int index, UnityAction action)
+    {
+        //AsyncOperation = SceneManager.LoadScene(index);
+
+    }
+}
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private const string ISATTACK = "IsAttack";
+
     private Slap slap;
 
     [SerializeField] private float timeToNextAttack;
     [SerializeField] private bool canAttack = true;
 
+    [Header("Slap")]
+    [SerializeField] private Transform slapHandlerTransform;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
     private void Start()
     {
         slap = GetComponentInChildren<Slap>();
@@ -17,6 +24,7 @@ public class PlayerAttack : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0) && canAttack)
         {
+            animator.SetTrigger(ISATTACK);
             StartCoroutine(Attack());
         }
 
@@ -36,6 +44,10 @@ public class PlayerAttack : MonoBehaviour
 
     public void ChangeSlap(Slap newSlap)
     {
+        Destroy(slapHandlerTransform.GetChild(0));
+
+        Instantiate(slap.gameObject, slapHandlerTransform);
+
         slap = newSlap;
     }
 }
