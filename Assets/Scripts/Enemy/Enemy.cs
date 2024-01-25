200a5fdde3a3dfad294064df16994a3e9b387ca4
Dmitriy Storozhuk <dreymentov@mail.ru>
2024-01-25 17:01:02 +0300

    some refactoring

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -13,23 +13,33 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        GetRandomTarget();
     }
 
     private void Update()
     {
         if(enemy == null && (target - transform.position).magnitude < 1f)
         {
-            target = new Vector3(Random.Range(transform.position.x - 10, transform.position.x + 10),
-                transform.position.y,
-                Random.Range(transform.position.x - 10, transform.position.x + 10));
+            GetRandomTarget();
         }
 
         Vector3 direction = (target - transform.position).normalized;
-
-        characterController.Move(direction * speed * Time.deltaTime);
+        Move(direction);
         transform.forward = new Vector3(direction.x, 0f, direction.z);
     }
 
+
+    private void Move(Vector3 direction)
+    {
+        characterController.Move(direction * speed * Time.deltaTime);
+    }
+
+    private void GetRandomTarget()
+    {
+        target = new Vector3(Random.Range(transform.position.x - 10, transform.position.x + 10),
+            transform.position.y,
+            Random.Range(transform.position.x - 10, transform.position.x + 10));
+    }
 
 
 }
