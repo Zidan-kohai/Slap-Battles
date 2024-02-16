a40e53c506bafccf782d080a9e23af69f5c74fd1
Zayniddin Kobilov <zidankohai@gmail.com>
2024-02-16 14:12:52 +0300

    diamond displayed

--- Assets/Scripts/UI/HubUI.cs
+++ Assets/Scripts/UI/HubUI.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class HubUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI slapCountText;
+    [SerializeField] private TextMeshProUGUI DiamondCountText;
 
     private void Start()
     {
         slapCountText.text = Geekplay.Instance.PlayerData.money.ToString();
+        DiamondCountText.text = Geekplay.Instance.PlayerData.DiamondMoney.ToString();
     }
 }
