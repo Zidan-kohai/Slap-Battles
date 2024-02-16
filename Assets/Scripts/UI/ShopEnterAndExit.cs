10c48572b6cfa8c2f76e0c720873959c14afead7
Zayniddin Kobilov <zidankohai@gmail.com>
2024-02-16 10:32:59 +0300

    some edit on inspector

--- Assets/Scripts/UI/ShopEnterAndExit.cs
+++ Assets/Scripts/UI/ShopEnterAndExit.cs
@@ -1,4 +1,5 @@
 using CMF;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopEnterAndExit : MonoBehaviour
@@ -7,6 +8,7 @@ public class ShopEnterAndExit : MonoBehaviour
     [SerializeField] private GameObject cinemashine;
     [SerializeField] private GameObject shopObject;
 
+    [SerializeField] List<GameObject> GOToDisableWhenOpenShop;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.layer == 7)
@@ -21,6 +23,11 @@ public class ShopEnterAndExit : MonoBehaviour
         cinemashine.SetActive(true);
         shopObject.SetActive(true);
 
+        foreach(GameObject go in GOToDisableWhenOpenShop)
+        {
+            go.SetActive(false);
+        }
+
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
@@ -30,5 +37,10 @@ public class ShopEnterAndExit : MonoBehaviour
         playerMover.enabled = true;
         cinemashine.SetActive(false);
         shopObject.SetActive(false);
+
+        foreach (GameObject go in GOToDisableWhenOpenShop)
+        {
+            go.SetActive(true);
+        }
     }
 }
