00b4ba53428d2a88c4a21fa0cf7094ebb8eee2a6
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-03-05 13:30:23 +0300

    fix null ref on enemies in some mode and on player on theif mode, and change attack type of player

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
@@ -20,6 +21,10 @@ public class PlayerAttack : MonoBehaviour
     [Header("Attack")]
     [SerializeField] private LayerMask enemyLayer;
     [SerializeField] private float attackDistanese;
+    public int RaycastCount = 10;
+    public float MaxAngle = 60f;
+    public float RaycastDistance = 2f;
+    public Vector3 Delta;
 
     [Header("Components")]
     [SerializeField] protected Player player;
@@ -61,9 +66,7 @@ public class PlayerAttack : MonoBehaviour
     {
         canAttack = false;
         animator.SetTrigger("Attack");
-        yield return new WaitForSeconds(0.6f);
-        slapAudio.Play();
-        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(0.4f);
         Attack();
 
 
@@ -74,35 +77,60 @@ public class PlayerAttack : MonoBehaviour
 
     public virtual void Attack()
     {
-        RaycastHit hit;
+        CastRaycasts();
+    }
+    private void CastRaycasts()
+    {
+        float startAngle = -MaxAngle / 2;
 
-        if (Physics.SphereCast(slapRaycaster.position - slapRaycaster.forward * 0.2f, 0.5f, slapRaycaster.forward, out hit, attackDistanese, enemyLayer, QueryTriggerInteraction.Ignore))
+        float angleStep = MaxAngle / (RaycastCount - 1);
+
+        for (int i = 0; i < RaycastCount; i++)
         {
-            slapCount++;
-            float attackPower = slap.AttackPower;
+            float currentAngle = startAngle + angleStep * i;
 
-            if (Geekplay.Instance.BuffIncreasePower)
-                attackPower *= 2; 
+            Vector3 raycastDirection = Quaternion.Euler(0, currentAngle, 0) * transform.forward;
 
-            hit.collider.gameObject.GetComponent<Enemy>().GetDamage(attackPower, slapRaycaster.forward, out bool isDeath, out int GettedSlap);
-
-            if(slap.GetSlapPowerType() == SlapPowerType.DoubleSlap || Geekplay.Instance.BuffDoubleSlap)
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Delta, raycastDirection, out hit, RaycastDistance, enemyLayer, QueryTriggerInteraction.Ignore))
             {
-                GettedSlap *= 2;
+                Debug.Log("Raycast hit at angle " + currentAngle + " degrees. Hit object: " + hit.collider.gameObject.name);
+
+                OnFindEnemyWhenAttack(hit.collider.GetComponent<Enemy>()); 
+                slapAudio.Play();
             }
-
-            player.SetStolenSlaps(GettedSlap);
-            eventManager.InvokeChangeMoneyEvents(GettedSlap);
-            OnSuccesAttack();
-
-            if(slapCount % 20 == 0 && Geekplay.Instance.currentMode != Modes.Boss)
+            else
             {
-                UIDiamond.Run();
-                eventManager.InvokeChangeDiamondEvents(1);
+                Debug.Log("Raycast at angle " + currentAngle + " degrees didn't hit anything");
             }
         }
     }
 
+    private void OnFindEnemyWhenAttack(Enemy enemy)
+    {
+        slapCount++;
+        float attackPower = slap.AttackPower;
+
+        if (Geekplay.Instance.BuffIncreasePower)
+            attackPower *= 2;
+
+        enemy.GetDamage(attackPower, slapRaycaster.forward, out bool isDeath, out int GettedSlap);
+
+        if (slap.GetSlapPowerType() == SlapPowerType.DoubleSlap || Geekplay.Instance.BuffDoubleSlap)
+        {
+            GettedSlap *= 2;
+        }
+
+        player.SetStolenSlaps(GettedSlap);
+        eventManager.InvokeChangeMoneyEvents(GettedSlap);
+        OnSuccesAttack();
+
+        if (slapCount % 20 == 0 && Geekplay.Instance.currentMode != Modes.Boss)
+        {
+            UIDiamond.Run();
+            eventManager.InvokeChangeDiamondEvents(1);
+        }
+    }
     protected virtual void OnSuccesAttack()
     {
 
@@ -116,11 +144,21 @@ public class PlayerAttack : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
+        // Начинаем с угла, равного отрицательной половине максимального угла
+        float startAngle = -MaxAngle / 2;
 
-        Gizmos.DrawRay(slapRaycaster.position - slapRaycaster.forward * 0.2f, slapRaycaster.forward * attackDistanese);
+        // Вычисляем шаг изменения угла для каждого рейкаста
+        float angleStep = MaxAngle / (RaycastCount - 1);
 
-        Gizmos.DrawWireSphere(slapRaycaster.position - slapRaycaster.forward * 0.2f, 0.5f);
-        Gizmos.DrawWireSphere(slapRaycaster.position + slapRaycaster.forward * attackDistanese - slapRaycaster.forward * 0.2f, 0.5f);
+        for (int i = 0; i < RaycastCount; i++)
+        {
+            // Вычисляем угол для текущего рейкаста
+            float currentAngle = startAngle + angleStep * i;
+
+            // Вычисляем направление для броска рейкаста на основе угла
+            Vector3 raycastDirection = Quaternion.Euler(0, currentAngle, 0) * transform.forward;
+            Debug.DrawRay(transform.position + Delta, raycastDirection * RaycastDistance);
+        }
     }
 
     private void OnDisable()
