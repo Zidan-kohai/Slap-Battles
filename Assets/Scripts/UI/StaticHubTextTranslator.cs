20b15c13d1ce8726d6244c41644e120f11115770
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-03-07 17:21:29 +0300

    hub translate is ready need test

--- Assets/Scripts/UI/StaticHubTextTranslator.cs
+++ Assets/Scripts/UI/StaticHubTextTranslator.cs
@@ -44,6 +44,7 @@ public class StaticHubTextTranslator : MonoBehaviour
     [SerializeField] private TextMeshPro leaderboardDescription;
     [SerializeField] private TextMeshProUGUI leaderboardName;
     [SerializeField] private TextMeshProUGUI otherGamesIconText;
+    [SerializeField] private TextMeshProUGUI otherGamesHeaderText;
     [SerializeField] private TextMeshProUGUI otherGamesButtonIconText;
     [SerializeField] private TextMeshProUGUI speedKeyPauseText;
     [SerializeField] private TextMeshProUGUI speedKeyTelegramText;
@@ -57,15 +58,157 @@ public class StaticHubTextTranslator : MonoBehaviour
     {
         if(Geekplay.Instance.language == "ru")
         {
+            portalStandartText.text = "Стандартный";
+            portalOnePunchText.text = "Один удар";
+            portalBattleRoyalText.text = "Королевская битва";
+            portalTheifText.text = "Вор шлепков";
+            portalAceText.text = "Леденой режим";
+            portalBossText.text = "Босс";
+            portalTimeText.text = "Режим таймер";
+
+
+            skineShopMainHeaderText.text = "Магазин скинов";
+            skineShopHeaderText.text = "Магазин скинов";
+            manGenderText.text = "парень";
+            womanGenderText.text = "девушка";
+            buffText.text = "Здоровья";
+
+
+            slapShopMainHeaderText.text = "Магазин шлепков";
+            slapShopHeaderText.text = "Магазин шлепков";
+
+
+            inAppShopMainHeaderText.text = "Ресурсы";
+            inAppShopHeaderText.text = "Ресурсы";
+            item1Cost.text = "2 ян";
+            item2Cost.text = "15 ян";
+            item3Cost.text = "35 ян";
+            item4Cost.text = "50 ян";
+            item5Cost.text = "80 ян";
+            item6Cost.text = "5 ян";
+            item7Cost.text = "20 ян";
+            item8Cost.text = "40 ян";
+            item9Cost.text = "60 ян";
+            item10Cost.text = "90 ян";
+            item11Cost.text = "100 ян";
+            item11SpecialText.text = "лучшее";
+
+
+            leaderboardDescription.text = "описание игры";
+            leaderboardName.text = "Лидеры";
+            otherGamesIconText.text = "другие игры";
+            otherGamesHeaderText.text = "наши игры";
+            otherGamesButtonIconText.text = "другие игры";
+            speedKeyPauseText.text = "нажмите <<Tab>>";
+            speedKeyTelegramText.text = "нажмите <<Е>>";
+            speedKeyInAppShoptext.text = "нажмите <<Ш>>";
+            speedKeyIncreasePowerText.text = "нажмите <<З>>";
+            speedKeyIncreaseSpeedText.text = "нажмите <<Ч>>";
+            speedKeyIncreaseHPText.text = "нажмите <<Р>>";
+            speedKeyDoubleSlapText.text = "нажмите <<А>>";
 
         }
         if (Geekplay.Instance.language == "en")
         {
+            portalStandartText.text = "Standard";
+            portalOnePunchText.text = "One Punch";
+            portalBattleRoyalText.text = "Battle Royal";
+            portalTheifText.text = "Slap Thief";
+            portalAceText.text = "Ice mode";
+            portalBossText.text = "Boss";
+            portalTimeText.text = "Timer mode";
 
+
+            skineShopMainHeaderText.text = "Skin Store";
+            skineShopHeaderText.text = "Skin Store";
+            manGenderText.text = "guy";
+            womanGenderText.text = "girl";
+            buffText.text = "Health";
+
+
+            slapShopMainHeaderText.text = "Slap Shop";
+            slapShopHeaderText.text = "Slap Shop";
+
+
+            inAppShopMainHeaderText.text = "Resources";
+            inAppShopHeaderText.text = "Resources";
+            item1Cost.text = "2 yan";
+            item2Cost.text = "15 yan";
+            item3Cost.text = "35 yan";
+            item4Cost.text = "50 yan";
+            item5Cost.text = "80 yan";
+            item6Cost.text = "5 yan";
+            item7Cost.text = "20 yan";
+            item8Cost.text = "40 yan";
+            item9Cost.text = "60 yan";
+            item10Cost.text = "90 yan";
+            item11Cost.text = "100 yan";
+            item11SpecialText.text = "Top";
+
+
+            leaderboardDescription.text = "game description";
+            leaderboardName.text = "Leaders";
+            otherGamesIconText.text = "other games";
+            otherGamesHeaderText.text = "our games";
+            otherGamesButtonIconText.text = "other games";
+            speedKeyPauseText.text = "press <<Tab>>";
+            speedKeyTelegramText.text = "press <<T>>";
+            speedKeyInAppShoptext.text = "press <<I>>";
+            speedKeyIncreasePowerText.text = "press <<P>>";
+            speedKeyIncreaseSpeedText.text = "press <<X>>";
+            speedKeyIncreaseHPText.text = "press <<H>>";
+            speedKeyDoubleSlapText.text = "press <<F>>";
         }
         if (Geekplay.Instance.language == "tr")
         {
+            portalStandartText.text = "Standart";
+            portalOnePunchText.text = "Tek Yumruk";
+            portalBattleRoyalText.text = "Battle Royal";
+            portalTheifText.text = "Tokat Hirsizi";
+            portalAceText.text = "Buz modu";
+            portalBossText.text = "Patron";
+            portalTimeText.text = "Zamanlayici modu";
 
+
+            skineShopMainHeaderText.text = "Dis Gorunum Magazasi";
+            skineShopHeaderText.text = "Dis Gorunum Magazasi";
+            manGenderText.text = "erkek";
+            womanGenderText.text = "kiz";
+            buffText.text = "Saglik";
+
+
+            slapShopMainHeaderText.text = "Tokat Magazasi";
+            slapShopHeaderText.text = "Tokat Magazasi";
+
+
+            inAppShopMainHeaderText.text = "Kaynaklar";
+            inAppShopHeaderText.text = "Kaynaklar";
+            item1Cost.text = "2 yan";
+            item2Cost.text = "15 yan";
+            item3Cost.text = "35 yan";
+            item4Cost.text = "50 yan";
+            item5Cost.text = "80 yan";
+            item6Cost.text = "5 yan";
+            item7Cost.text = "20 yan";
+            item8Cost.text = "40 yan";
+            item9Cost.text = "60 yan";
+            item10Cost.text = "90 yan";
+            item11Cost.text = "100 yan";
+            item11SpecialText.text = "sey";
+
+
+            leaderboardDescription.text = "oyun aciklamasi";
+            leaderboardName.text = "Liderler";
+            otherGamesIconText.text = "diger oyunlar";
+            otherGamesHeaderText.text = "oyunlarimiz";
+            otherGamesButtonIconText.text = "diger oyunlar";
+            speedKeyPauseText.text = "<<Tab>> tusuna basin";
+            speedKeyTelegramText.text = "<<T>> tusuna basin";
+            speedKeyInAppShoptext.text = "<<I>> tusuna basin";
+            speedKeyIncreasePowerText.text = "<<P>> tusuna basin";
+            speedKeyIncreaseSpeedText.text = "<<X>> tusuna basin";
+            speedKeyIncreaseHPText.text = "<<H>> tusuna basin";
+            speedKeyDoubleSlapText.text = "<<F>> tusuna basin";
         }
     }
 }
