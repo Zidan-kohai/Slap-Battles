5219c0d5ee01f36e9469cd954997c65e6a2ff481
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-03-12 15:42:29 +0300

    some improvement. and fix bag with falling from the island use wall superPower

--- Assets/Scripts/Other/DeathCollider.cs
+++ Assets/Scripts/Other/DeathCollider.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class DeathCollider : MonoBehaviour
 {
+    public Player player;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.TryGetComponent<IHealthObject>(out IHealthObject healthObject))
         {
             healthObject.Death(false);
+        }
+        else if(other.gameObject.layer == 7)
+        {
+            player.Death();
         }
     }
 }
